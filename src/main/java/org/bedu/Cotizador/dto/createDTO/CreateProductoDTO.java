8c4403c71f46975068eef0c77f9145bcb47c7871
Dayan <carlosdayanmlopez@gmail.com>
2023-12-05 23:15:22 -0600

    Se agregaron validaciones para crear un producto y que el precio en el modelo producto muestr los dos decimales

--- src/main/java/org/bedu/Cotizador/dto/createDTO/CreateProductoDTO.java
+++ src/main/java/org/bedu/Cotizador/dto/createDTO/CreateProductoDTO.java
@@ -1,15 +1,27 @@
 package org.bedu.Cotizador.dto.createDTO;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CreateProductoDTO {
+    @NotBlank(message = "El nombre no puede estar en blanco")
     private String nombre;
+
     private String sku;
+
+    @NotNull
+    @Digits(integer = 200, fraction = 2, message = "El precio debe tener hasta dos dígitos decimales")
+    @Positive(message = "El precio debe ser mayor a 0")
     private double precio;
+    @Positive (message = "El stock debe ser un número positivo")
     private int stock;
+    @Size (max = 150, message = "La descripción debe tener como máximo 150 caracteres")
     private String descripcion;
+    @NotBlank(message = "La categoría no puede estar en blanco")
     private String categoria;
+    @NotBlank(message = "La marca no puede estar en blanco")
     private String marca;
+    @NotBlank(message = "El modelo no puede estar en blanco")
     private String modelo;
 }
--- src/main/java/org/bedu/Cotizador/model/Producto.java
+++ src/main/java/org/bedu/Cotizador/model/Producto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.NumberFormat;
 
 @Getter
 @Setter
@@ -23,12 +24,13 @@ public class Producto {
     private String sku;
 
     @Column(nullable = false)
+    @NumberFormat(pattern = "#0.00")
     private double precio;
 
     @Column(nullable = false)
     private int stock;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 150)
     private String descripcion;
 
     @Column(nullable = false)
