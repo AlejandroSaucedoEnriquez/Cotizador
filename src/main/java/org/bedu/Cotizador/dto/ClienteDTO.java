76e49dd65349f73464fd6c2640a4a9500358ca9e
Alejandro SE <alejandrosaucedoenriquez@gmail.com>
2023-12-11 17:21:19 -0600

    agregeg algunos shemas faltan los demas

--- src/main/java/org/bedu/Cotizador/dto/ClienteDTO.java
+++ src/main/java/org/bedu/Cotizador/dto/ClienteDTO.java
@@ -1,14 +1,21 @@
 package org.bedu.Cotizador.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class ClienteDTO {
+    @Schema(description = "Identificador del cliente", example = "20")
     private long id;
+    @Schema(description = "Nombre del cliente", example = "Rodrigo")
     private String nombre;
+    @Schema(description = "apellido del cliente", example = "Chavez Ramos")
     private String apellido;
+    @Schema(description = "direccion del cliente", example = "Av. Vallarta 1532")
     private String direccion;
+    @Schema(description = "correo electronico del cliente", example = "ejemplo@gmail.com")
     private String email;
+    @Schema(description = "telefono del cliente", example = "3315255110")
     private String telefono;
 }
--- src/main/java/org/bedu/Cotizador/dto/createDTO/CreateClienteDTO.java
+++ src/main/java/org/bedu/Cotizador/dto/createDTO/CreateClienteDTO.java
@@ -1,5 +1,6 @@
 package org.bedu.Cotizador.dto.createDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -7,6 +8,7 @@ import lombok.Data;
 
 @Data
 public class CreateClienteDTO {
+    @Schema(description = "Nombre del cliente", example = "Rodrigo")
     @NotBlank(message = "El nombre no puede estar en blanco")
     private String nombre;
     @NotBlank(message = "El apellido no puede estar en blanco")
