cd48a29adef4f39c8d995291c91c740e32cf4ad5
Alejandro SE <alejandrosaucedoenriquez@gmail.com>
2023-12-10 20:49:36 -0600

    Se agrego la carpeta exception y clase RuntimeException.java con su contenido y se modifico ExceptionAdvice.java

--- src/main/java/org/bedu/Cotizador/config/ExceptionAdvice.java
+++ src/main/java/org/bedu/Cotizador/config/ExceptionAdvice.java
@@ -1,28 +1,34 @@
 package org.bedu.Cotizador.config;
 
 import org.bedu.Cotizador.dto.ErrorDTO;
-import org.springframework.http.HttpStatus;
+import org.bedu.Cotizador.exception.RuntimeException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.LinkedList;
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorDTO validationErrors(MethodArgumentNotValidException ex) {
+    public ErrorDTO validationError(MethodArgumentNotValidException ex) {
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-        List<String> errors = new LinkedList<>();
+        List<String> errors = fieldErrors.stream().map(x -> x.getDefaultMessage()).toList();
+        return new ErrorDTO("ERR_VALID", "Hubo un error al procesar los datos de entrada", errors);
+    }
 
-        for (FieldError fieldError : fieldErrors) {
-            errors.add(fieldError.getDefaultMessage());
-        }
-        return new ErrorDTO("ERR_VALID", "Hubo un error al validar los datos de entrada", errors);
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorDTO applicationError(RuntimeException ex) {
+        return new ErrorDTO(ex.getCode(), ex.getMessage(), ex.getDetails());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ErrorDTO unknownError(Exception ex) {
+        log.error(ex.getMessage());
+        return new ErrorDTO("ERR_UNKNOWN", "Ocurrió un error inesperado", null);
     }
 }
